bd0d9a0bdf24eb5ab26d643e675062c3c4a4d98d
Yernar Mendigarin <yernar.m90@gmail.com>
2018-10-22 15:07:59 +0600

    first kotlin code!

--- app/src/main/java/com/yernarkt/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/yernarkt/dinnerdecider/MainActivity.kt
@@ -3,12 +3,27 @@ package com.yernarkt.dinnerdecider
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private val foodList = arrayListOf("Бешпармак", "KFC", "Burger King", "Pizza", "Sushi")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        btnAddFood.setOnClickListener {
+            val newFood = tvAddFood.text.toString()
+            if(newFood != "") {
+                foodList.add(newFood)
+                tvAddFood.text.clear()
+            }
+        }
+
+        btnDecide.setOnClickListener {
+            val random = Random()
+            val randomFoodIndex = random.nextInt(foodList.count())
+            tvDinnerDeciderTitle.text = foodList[randomFoodIndex]
+        }
     }
 }
